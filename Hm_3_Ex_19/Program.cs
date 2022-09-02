4e061db437a07ce47bd36d8a31e91163659c3a26
Leon <turkov.leon-2@yandex.ru>
2022-09-02 16:47:19 +0300

    Added Ex. 10

---
+++ Hm_3_Ex_19/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Input number");
+string? x = Console.ReadLine();
+
+void WriteResult (bool Result)
+{
+    if (Result != true)
+    {
+        Console.WriteLine("Не является полендромом");
+    }
+    else
+    {
+        Console.WriteLine("Является полендромом");
+    } 
+}
+bool CheckPolindrom (string input)
+{
+    int index = input.Length - 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        bool check = input[i] == input[index];
+        index--;
+        if (check == false)
+        {
+            return check;
+        }
+    }
+    return true;  
+}
+
+
+WriteResult(CheckPolindrom(x));
+    
+    
